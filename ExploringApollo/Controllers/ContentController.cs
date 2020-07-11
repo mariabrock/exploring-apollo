8a02f4c740ae038a1b49ad139d5402e0b5350bcd
Maria <mariabrock414@gmail.com>
2020-07-11 10:00:05 -0500

    finished backend setup

--- ExploringApollo/Controllers/ContentController.cs
+++ ExploringApollo/Controllers/ContentController.cs
@@ -7,9 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ExploringApollo.Controllers
 {
-    [Route("api/[user]")]
+    [Route("api/[content]")]
     [ApiController]
-    public class UserController : ControllerBase
+    public class ContentController : ControllerBase
     {
     }
 }
